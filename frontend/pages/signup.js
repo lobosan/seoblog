92d9ab2451c772994ebef971cfb0cc57dbc52dee
Santiago Galindo <sp.galindoh@gmail.com>
2020-04-14 19:14:44 -0500

    signup action and alerts

--- frontend/pages/signup.js
+++ frontend/pages/signup.js
@@ -1,12 +1,15 @@
 import Layout from '../components/Layout';
 import SignupComponent from '../components/auth/SignupComponent';
-import Link from 'next/link';
 
 const Signup = () => {
   return (
     <Layout>
-      <h2>Signup page</h2>
-      <SignupComponent />
+      <h2 className="text-center pt-4 pb-4">Signup</h2>
+      <div className="row">
+        <div className="col-md-4 offset-md-4">
+          <SignupComponent />
+        </div>
+      </div>
     </Layout>
   );
 };
