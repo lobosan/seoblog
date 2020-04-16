c87965d1d63e9302edbb5374a2ce5099545d1876
Santiago Galindo <sp.galindoh@gmail.com>
2020-04-15 19:25:57 -0500

    admin and user dashboard

--- frontend/components/auth/SigninComponent.js
+++ frontend/components/auth/SigninComponent.js
@@ -29,7 +29,11 @@ const SigninComponent = () => {
         setValues({ ...values, error: data.error, loading: false });
       } else {
         authenticate(data, () => {
-          Router.push(`/`);
+          if (isAuth() && isAuth().role === 1) {
+            Router.push('/admin');
+          } else {
+            Router.push('/user');
+          }
         });
       }
     });
